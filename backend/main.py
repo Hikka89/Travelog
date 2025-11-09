21db09db30ca87e296afd9d0f2c3655d0713e8bb
Hikka89 <kostya10062005@mail.ru>
2025-11-09 23:57:38 +0300

    websocket server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pymongo import MongoClient
 
+from ConnectionManager import ConnectionManager
 from DB.PlacesRepository import PlacesRepository
 from DB.UserRepository import UserRepository
 from Routes.PlacesRoutes import PlacesRoutes
@@ -16,6 +17,8 @@ try:
 except Exception as e:
     print(f"MongoDB error: {e}")
 
+manager = ConnectionManager()
+
 user_repo = UserRepository(db)
 places_repo = PlacesRepository(db)
 user_routes = UserRoutes(user_repo)
@@ -23,3 +26,14 @@ places_routes = PlacesRoutes(places_repo)
 
 app.include_router(places_routes.router, prefix="/api/place")
 app.include_router(user_routes.router, prefix="/api")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except Exception:
+        pass
+    finally:
+        manager.disconnect(websocket)
